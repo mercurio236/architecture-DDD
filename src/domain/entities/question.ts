96fa5ae99b11f88fc74c375a4a164a60c4ed1ffc
Arley Souto <macx15@hotmail.com>
2024-03-26 13:29:34 -0300

    feat: criando arquivo de object slug

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,7 +1,9 @@
 import { randomUUID } from 'node:crypto'
+import { Slug } from './value-objects/slug'
 
 interface QuestionsProps {
   title: string
+  slug: Slug
   content: string
   authorId: string
 }
@@ -9,12 +11,14 @@ interface QuestionsProps {
 export class Questions {
   public id: string
   public title: string
+  public slug: Slug
   public content: string
   public authorId: string
 
   constructor(props: QuestionsProps, id?: string) {
     this.title = props.title
     this.content = props.content
+    this.slug = props.slug
     this.authorId = props.authorId
     this.id = id ?? randomUUID()
   }
