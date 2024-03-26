a9b6e1bb0e9ddaa0ee30c13f5b01203a47c59109
Arley Souto <macx15@hotmail.com>
2024-03-26 12:54:27 -0300

    feat: dependencias externas

--- src/domain/use-cases/answer-question.test.ts
+++ src/domain/use-cases/answer-question.test.ts
@@ -1,10 +1,17 @@
 import {expect, test} from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
+import { AnswerRepository } from '../repositories/answer-repository'
+import { Answer } from '../entities/answer'
 
-test('create an answer', () =>{
-    const answerQuestion = new AnswerQuestionUseCase()
+const fakeAnswerRepository: AnswerRepository = {
+    create: async (answer: Answer) => {
+        return
+    }
+}
+test('create an answer', async () =>{
+    const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
 
-    const answer = answerQuestion.execute({
+    const answer = await answerQuestion.execute({
         questionId:'1',
         instructorId:'1',
         content:'nova resposta'
