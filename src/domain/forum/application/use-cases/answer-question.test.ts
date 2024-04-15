0e619aaf941c76b998b812b494b765ae07a0ebff
Arley Souto <macx15@hotmail.com>
2024-04-15 12:51:32 -0300

    style: refatorado organização de pastas

--- src/domain/forum/application/use-cases/answer-question.test.ts
+++ src/domain/forum/application/use-cases/answer-question.test.ts
@@ -1,7 +1,7 @@
 import {expect, test} from 'vitest'
 import { AnswerQuestionUseCase } from './answer-question'
-import { AnswerRepository } from '../repositories/answer-repository'
-import { Answer } from '../entities/answer'
+import { AnswerRepository } from '../../../repositories/answer-repository'
+import { Answer } from '../../enterprise/entities/answer'
 
 const fakeAnswerRepository: AnswerRepository = {
     create: async (answer: Answer) => {
