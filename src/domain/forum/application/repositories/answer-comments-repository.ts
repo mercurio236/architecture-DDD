ccb264ed8e7f2be03527a797d74a9e7a223ae32a
Arley Souto <macx15@hotmail.com>
2024-04-16 15:13:40 -0300

    feat: delete answer

--- src/domain/forum/application/repositories/answer-comments-repository.ts
+++ src/domain/forum/application/repositories/answer-comments-repository.ts
@@ -1,6 +1,7 @@
-import { AnswerComment } from "../../enterprise/entities/answer-comment";
+import { AnswerComment } from '../../enterprise/entities/answer-comment'
 
-
-export interface AnswerCommentsRepository{
-    create(answer: AnswerComment ): Promise<void>
+export interface AnswerCommentsRepository {
+  findById(id: string): Promise<AnswerComment | null>
+  create(answer: AnswerComment): Promise<void>
+  delete(answer: AnswerComment): Promise<void>
 }
--- test/respositories/in-memory-answer-comments-repository.ts
+++ test/respositories/in-memory-answer-comments-repository.ts
@@ -4,10 +4,26 @@ import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment
 
 export class InMemoryAnswerCommentRepository implements AnswerCommentsRepository {
   public items: AnswerComment[] = []
+
+  async findById(id: string) {
+    const answerComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!answerComment) {
+      return null
+    }
+
+    return answerComment
+  }
   
 
   async create(answerComment: AnswerComment): Promise<void> {
     this.items.push(answerComment)
+  }
+
+  async delete(answerComment: AnswerComment) {
+    const itemIndex = this.items.findIndex((item) => item.id === answerComment.id)
+
+    this.items.splice(itemIndex, 1)
   }
 
   
