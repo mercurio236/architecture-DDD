b061cde68ac4efe816e85893d520c06ad8e604e8
Arley Souto <macx15@hotmail.com>
2024-04-16 14:50:43 -0300

    feat: comentario da resposta

---
+++ test/respositories/in-memory-answer-comments-repository.ts
@@ -0,0 +1,14 @@
+import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
+import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
+
+
+export class InMemoryAnswerCommentRepository implements AnswerCommentsRepository {
+  public items: AnswerComment[] = []
+  
+
+  async create(answerComment: AnswerComment): Promise<void> {
+    this.items.push(answerComment)
+  }
+
+  
+}
