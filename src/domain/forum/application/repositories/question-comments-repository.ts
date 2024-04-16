c92ab9d4eb35bc00d4f75f52c5d27753d13a5811
Arley Souto <macx15@hotmail.com>
2024-04-16 15:29:18 -0300

    feat: lista comentarios da pergunta

--- src/domain/forum/application/repositories/question-comments-repository.ts
+++ src/domain/forum/application/repositories/question-comments-repository.ts
@@ -1,9 +1,13 @@
-import { Questions } from "../../enterprise/entities/question";
-import { QuestionComment } from "../../enterprise/entities/question-comment";
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { Questions } from '../../enterprise/entities/question'
+import { QuestionComment } from '../../enterprise/entities/question-comment'
 
-
-export interface QuestionCommentsRepository{
-    findById(id: string ): Promise<QuestionComment | null>
-    create(question: QuestionComment ): Promise<void>
-    delete(question: QuestionComment ): Promise<void>
+export interface QuestionCommentsRepository {
+  findById(id: string): Promise<QuestionComment | null>
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams
+  ): Promise<QuestionComment[]>
+  create(question: QuestionComment): Promise<void>
+  delete(question: QuestionComment): Promise<void>
 }
