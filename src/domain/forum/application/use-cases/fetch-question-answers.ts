36d62937e814c0b2adc00656b87d2792f93076a2
Arley Souto <macx15@hotmail.com>
2024-04-16 12:42:01 -0300

    feat: listar respostas

---
+++ src/domain/forum/application/use-cases/fetch-question-answers.ts
@@ -0,0 +1,29 @@
+import { AnswerRepository } from '../repositories/answer-repository'
+import { Answer } from '../../enterprise/entities/answer'
+
+interface FetchQuestionAnswersUseCaseRequest {
+  questionId: string
+  page: number
+}
+
+interface FetchQuestionAnswersUseCaseResponse {
+  answers: Answer[]
+}
+
+export class FetchQuestionAnswersUseCase {
+  constructor(private answersRepository: AnswerRepository) {}
+
+  async execute({
+    questionId,
+    page,
+  }: FetchQuestionAnswersUseCaseRequest): Promise<FetchQuestionAnswersUseCaseResponse> {
+    const answers = await this.answersRepository.findManyByQuestionId(
+      questionId,
+      { page }
+    )
+
+    return {
+      answers,
+    }
+  }
+}
--- test/respositories/in-memory-answers-repository.ts
+++ test/respositories/in-memory-answers-repository.ts
@@ -1,35 +1,41 @@
-import { AnswerRepository } from "@/domain/forum/application/repositories/answer-repository"
-import { Answer } from "@/domain/forum/enterprise/entities/answer"
+import { PaginationParams } from '@/core/repositories/pagination-params'
+import { AnswerRepository } from '@/domain/forum/application/repositories/answer-repository'
+import { Answer } from '@/domain/forum/enterprise/entities/answer'
 
+export class InMemoryAnswerRepository implements AnswerRepository {
+  public items: Answer[] = []
 
-export class InMemoryAnswerRepository implements AnswerRepository{
-    public items: Answer[] = []
+  async findById(id: string) {
+    const answer = this.items.find((item) => item.id.toString() === id)
 
-    async findById(id: string){
-        const answer = this.items.find(item => item.id.toString() === id)
-
-        if(!answer){
-            return null
-        }
-
-        return answer
+    if (!answer) {
+      return null
     }
 
-    async save(answer: Answer) {
-        const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+    return answer
+  }
 
-        this.items[itemIndex] = answer
-    }
+  async save(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
 
+    this.items[itemIndex] = answer
+  }
 
-    async create(answer: Answer): Promise<void> {
-        this.items.push(answer)
-    }
+  async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
+    const answers = this.items
+      .filter((item) => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
 
-    async delete(answer: Answer) {
-        const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+    return answers
+  }
 
-        this.items.splice(itemIndex, 1)
-    }
+  async create(answer: Answer): Promise<void> {
+    this.items.push(answer)
+  }
 
+  async delete(answer: Answer) {
+    const itemIndex = this.items.findIndex((item) => item.id === answer.id)
+
+    this.items.splice(itemIndex, 1)
+  }
 }
