d5da9076aa4a7261aa3092f893c50cbc716ae27a
Arley Souto <macx15@hotmail.com>
2024-04-16 13:39:27 -0300

    feat: classe base de comentarios

--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,52 +1,28 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Optional } from '@/core/types/optional'
+import { Comment, CommentProps } from './comment'
 
-export interface QuestionCommentProps {
-  authorId: UniqueEntityID
+export interface QuesntionCommentProps extends CommentProps {
   questionId: UniqueEntityID
-  content: string
-  createdAt: Date
-  updatedAt?: Date
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps> {
-  get authorId() {
-    return this.props.authorId
-  }
-
-  get content() {
-    return this.props.content
-  }
-
-  get createdAt() {
-    return this.props.createdAt
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date()
-  }
-
-  set content(content: string) {
-    this.props.content = content
-    this.touch()
+export class QuesntionComment extends Comment<QuesntionCommentProps> {
+  get questionId() {
+    return this.props.questionId
   }
 
   static create(
-    props: Optional<QuestionCommentProps, 'createdAt'>,
+    props: Optional<QuesntionCommentProps, 'createdAt'>,
     id?: UniqueEntityID
   ) {
-    const questionComment = new QuestionComment(
+    const quesntionComment = new QuesntionComment(
       {
         ...props,
         createdAt: props.createdAt ?? new Date(),
       },
       id
     )
-    return questionComment
+    return quesntionComment
   }
 }
