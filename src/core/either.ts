f09dab3191cf1e61fbe83beee25f48c33950f4a6
Arley Souto <macx15@hotmail.com>
2024-04-17 13:35:53 -0300

    feat: testando classe de resposta

--- src/core/either.ts
+++ src/core/either.ts
@@ -1,22 +1,42 @@
 //error
-export class Left<L> {
+export class Left<L, R> {
   readonly value: L //ela nunca vai ser alterada após a sua inicialização
 
   constructor(value: L) {
     this.value = value
   }
+
+  //Metodos auxiliares
+
+  isRight(): this is Right<L, R> {
+    return false
+  }
+
+  isLeft(): this is Left<L, R> {
+    return true
+  }
 }
 
 //success
-export class Right<R> {
+export class Right<R, L> {
   readonly value: R //ela nunca vai ser alterada após a sua inicialização
 
   constructor(value: R) {
     this.value = value
   }
+
+  //Metodos auxiliares
+
+  isRight(): this is Right<L, R> {
+    return true
+  }
+
+  isLeft(): this is Left<L, R> {
+    return false
+  }
 }
 
-export type Either<L, R> = Left<L> | Right<R>
+export type Either<L, R> = Left<L, R> | Right<R, L>
 
 export const left = <L, R>(value: L): Either<L, R> => {
   return new Left(value)
