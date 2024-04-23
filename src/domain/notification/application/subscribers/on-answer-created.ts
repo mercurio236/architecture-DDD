660c0eda1daaac52d47fb2fc6430e1ff40ab77f9
Arley Souto <macx15@hotmail.com>
2024-04-23 14:27:14 -0300

    feat: enviando notificação no subscriber

--- src/domain/notification/application/subscribers/on-answer-created.ts
+++ src/domain/notification/application/subscribers/on-answer-created.ts
@@ -1,9 +1,14 @@
 import { DomainEvents } from '@/core/events/domain-events'
 import { EventHandler } from '@/core/events/event-handler'
+import { QuestionRepository } from '@/domain/forum/application/repositories/question-repository'
 import { AnswerCreatedEvent } from '@/domain/forum/enterprise/events/answer-created-event'
+import { SendNotificationUseCase } from '../use-cases/send-notification'
 
 export class OnAnswerCreated implements EventHandler {
-  constructor() {
+  constructor(
+    private questionsRepository: QuestionRepository,
+    private sendNotification: SendNotificationUseCase
+  ) {
     this.setupSubscriptions()
   }
 
@@ -14,5 +19,19 @@ export class OnAnswerCreated implements EventHandler {
     )
   }
 
-  private async sendNewAnswerNotification({ answer }: AnswerCreatedEvent) {}
+  private async sendNewAnswerNotification({ answer }: AnswerCreatedEvent) {
+    const question = await this.questionsRepository.findById(
+      answer.questionId.toString()
+    )
+
+    if (question) {
+      await this.sendNotification.execute({
+        recipientId: question.authorId.toString(),
+        title: `Nova resposta em "${question.title
+          .substring(0, 40)
+          .concat('...')}"`,
+        content: answer.except,
+      })
+    }
+  }
 }
